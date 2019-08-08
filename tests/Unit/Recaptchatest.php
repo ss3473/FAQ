1eb0989f618e3913f1a86b0915d653d230c2d8ce
sagar <ss3473@njit.edu>
2019-08-08 15:51:24 -0400

    added recaptcha v3 unit test.

---
+++ tests/Unit/Recaptchatest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Recaptchatest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testSave()
+
+        {
+            $recaptcha = new ReCaptcha($invalid);
+        }
+
+}
